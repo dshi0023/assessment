cc39530811809d5f6e5c4ca6ee182000e942c0fa
dshi0023 <dshi0023@student.monash.edu>
2025-08-23 12:46:09 +1000

    Created navbar and added bootstrap icons

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+    <div class="container">
+      <a class="navbar-brand fw-bold text-light" href="/">
+        <img
+          src="../assets/nutribridge-logo.png"
+          alt="NutriBridge Logo"
+          height="35"
+        />
+      </a>
+      <button
+        class="navbar-toggler text-light border"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#nav"
+        aria-controls="nav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="bi bi-list"></i>
+      </button>
+
+      <div id="nav" class="collapse navbar-collapse">
+        <ul
+          class="navbar-nav ms-auto nav-links fw-medium text-center text-lg-end"
+        >
+          <li class="nav-item">
+            <a class="nav-link text-light" href="/">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="/">Personalize</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="/">Recipes</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+.navbar-custom {
+  background-color: #819a91 !important;
+}
+.nav-links {
+  font-family: "Manrope", sans-serif;
+}
+.navbar-toggler:focus,
+.navbar-toggler:active {
+  outline: none !important;
+  box-shadow: none !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App.vue";
-
+import "./style.css";
 createApp(App).mount("#app");
