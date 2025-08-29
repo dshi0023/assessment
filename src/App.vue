8c7df90d3927c2a9a7e42dae04eb869f7f52120b
dshi0023 <dshi0023@student.monash.edu>
2025-08-29 11:33:09 +1000

    Added features section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 import Navbar from "./components/LandingPage/Navbar.vue";
 import HeroSection from "./components/LandingPage/HeroSection.vue";
+import FeaturesSection from "./components/LandingPage/FeaturesSection.vue";
 </script>
 
 <template>
   <div>
     <Navbar />
   </div>
-  <div class="border-bottom">
+  <div>
     <HeroSection />
+  </div>
+  <div>
+    <FeaturesSection />
   </div>
 </template>
---
+++ src/components/LandingPage/FeaturesSection.vue
@@ -0,0 +1,53 @@
+<script setup>
+import features from "../../data/features.json";
+</script>
+
+<template>
+  <div class="container-fluid p-3 p-sm-5 featuressection-container">
+    <h2 class="display-5 fw-bold text-center featuressection-header mb-4">
+      Why NutriBridge
+    </h2>
+
+    <div class="row g-3">
+      <div v-for="f in features" :key="f.id" class="col-12 col-md-6 col-xxl-4">
+        <div class="card h-100 card-container">
+          <div class="card-body py-4 d-flex flex-column align-items-center">
+            <div class="mb-3 card-custom-icon-box p-2 rounded shadow">
+              <i :class="f.icon" class="text-white card-custom-icon"></i>
+            </div>
+            <h5 class="card-title fw-semibold card-title text-center">
+              {{ f.title }}
+            </h5>
+            <p class="card-text text-dark-emphasis text-center card-info">
+              {{ f.text }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.featuressection-container {
+  background-color: #d1d8be;
+}
+.featuressection-header {
+  font-family: "Rubik", sans-serif;
+}
+.card-container {
+  background-color: #a7c1a8;
+}
+.card-title {
+  font-family: "Poppins", sans-serif;
+}
+.card-info {
+  font-family: "Manrope", sans-serif;
+}
+.card-custom-icon-box {
+  background-color: #819a91;
+}
+.card-custom-icon {
+  font-size: 3.5rem;
+}
+</style>
